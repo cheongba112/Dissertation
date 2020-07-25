964b8fa95579ef6730aa9ff2f6198a3b704b0ad7
CrayonGit <cheongba112@gmail.com>
2020-07-25 16:16:32 +0100

    new

---
+++ get_dataset.py
@@ -0,0 +1,55 @@
+'''
+This py file is customised for importing CACD2000 dataset
+Inspired by https://blog.csdn.net/woshicao11/article/details/78318156
+'''
+import torch
+from torch.utils.data import Dataset
+
+import os, re, pprint
+
+from PIL import Image
+from torchvision import transforms
+
+
+def get_img_list(root):
+    img_list = []
+    pattern = re.compile('([0-9]+)(\_)(.+)(\_[0-9]+)')
+
+    for img_name in os.listdir(root):
+        img_match = re.search(pattern, img_name)
+        
+        img_path = os.path.join(root, img_name)
+        img_age  = img_match.group(1)
+        img_id   = img_match.group(3)
+        
+        img_list.append([img_path, img_age, img_id])
+    
+    return img_list
+
+
+# pprint.pprint(get_img_list('./14'))
+
+
+class CACD_Dataset(Dataset):
+    def __init__(self, root):
+        self.img_list = get_img_list(root)
+
+    def __getitem__(self, idx):
+        img_path, img_age, img_id = self.img_list[idx]
+
+        img = Image.open(img_path)
+        trans = transforms.Compose([
+            transforms.Resize(250),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),  # from 0,1 to -1,1
+            ])
+        img = trans(img)
+
+        return img, img_age, img_id
+
+    def __len__(self):
+        return len(self.img_list)
+
+
+# cacd = CACD_Dataset('./14')
+# pprint.pprint(cacd[0][0].size())
--- models.py
+++ models.py
@@ -4,36 +4,31 @@ from matplotlib import pyplot as plt
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import Dataset, DataLoader
 
-from torchvision import transforms
+from torch.utils.data import DataLoader
 from torchvision.models import vgg19
 
-from PIL import Image
-
+from get_dataset import *
 
 # Create the dataset
-dataset = dset.ImageFolder(root='CACD2000',
-                           transform=transforms.Compose([
-                               transforms.ToTensor(),
-                               transforms.Normalize((0.5, 0.5, 0.5), 
-                                                    (0.5, 0.5, 0.5)),
-                           ]))
+dataset = CACD_Dataset('./14')
 
 # Create the dataloader
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=100,
                                          shuffle=True, num_workers=2)
 
 # Decide which device we want to run on
-device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # Models
 class Gen(nn.Module):
     def __init__(self):
         super(Gen, self).__init__()
-        self.embed = 
+        self.embed = nn.Embedding(100, 100)
         self.encode = nn.Sequential(
-            nn.Conv2d(3, )
+            nn.Conv2d(3, 64, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d()
         )
         self.deocde = nn.Sequential(
 
@@ -43,7 +38,6 @@ class Gen(nn.Module):
         latent_vec = self.encode(x)
         latent_vec = torch.cat((self.embed(con), x), -1)
         output = self.decode(latent_vec)
-        output = output.view(, )
         return output
 
 # class Age_Dis(nn.Module):
@@ -51,4 +45,12 @@ class Gen(nn.Module):
 # class Id_Dis(nn.Module):
 
 # class Age_Reg(nn.Module):
+
+
+
+
+# for (img, age, name) in dataset:
+#     print(age, name)
+
+
 
