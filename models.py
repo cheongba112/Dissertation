689d53593a46939f76349d00d00a208c1609cf8a
CrayonGit <cheongba112@gmail.com>
2020-07-25 16:30:20 +0100

    Update models.py

--- models.py
+++ models.py
@@ -24,25 +24,47 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 class Gen(nn.Module):
     def __init__(self):
         super(Gen, self).__init__()
-        self.embed = nn.Embedding(100, 100)
+        self.embed = nn.Embedding(80, 49)  # from 0 to 79 years old, 7 * 7 image
         self.encode = nn.Sequential(
             nn.Conv2d(3, 64, 3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d()
+            nn.MaxPool2d(2),
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),  # [1, 64, 7, 7]
         )
         self.deocde = nn.Sequential(
-
+            nn.ConvTranspose2d(65, 64, 3, stride=3),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 64, 3, stride=2),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 64, 3, stride=3),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 3, 3, stride=2),
+            nn.ReLU(True),
         )
 
     def forward(self, x, con):
         latent_vec = self.encode(x)
-        latent_vec = torch.cat((self.embed(con), x), -1)
+        latent_vec = torch.cat((torch.reshape(self.embed(con), (1, 1, 7, 7)), latent_vec), 1)
         output = self.decode(latent_vec)
+        output = output.view(3, 259, 259)
         return output
 
 # class Age_Dis(nn.Module):
 
-# class Id_Dis(nn.Module):
+class Id_Dis(nn.Module):
+    def __init__(self):
+        super(Id_Dis, self).__init__()
 
 # class Age_Reg(nn.Module):
 
@@ -53,4 +75,24 @@ class Gen(nn.Module):
 #     print(age, name)
 
 
+# --------------Rough Test-------------------
+# 200723
 
+# img = Image.open('test.jpg')
+# img2tensor = transforms.ToTensor()
+# img = img2tensor(img).resize_(1, 3, 250, 250)
+
+# # print(img.size())
+# # [3, 250, 250]
+
+# gen = Gen()
+# out = gen(img, torch.tensor([1]))
+# print(out)
+
+# # out = out.resize_(3, 259, 259)
+
+# unloader = transforms.ToPILImage()
+# out = unloader(out)
+# out.save('res.jpg')
+
+# --------------Training---------------------
