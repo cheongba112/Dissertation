0bc2191376886fe8b4b24b6eaf864a243db9c8c4
CrayonGit <cheongba112@gmail.com>
2020-08-01 05:33:16 +0100

    set batch size smaller

--- options.py
+++ options.py
@@ -2,6 +2,6 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--batch_size', required=False, type=int, default=100, help='batch size')
+parser.add_argument('--batch_size', required=False, type=int, default=20, help='batch size')
 
 opt = parser.parse_args()
