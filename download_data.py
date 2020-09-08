f7b7603add1dad3f0d001138dafe67e0b2efc270
CrayonGit <cheongba112@gmail.com>
2020-09-08 04:02:03 +0100

    set up 12

--- download_data.py
+++ download_data.py
@@ -1,21 +1,12 @@
-import os, argparse
+import argparse
 from download_gdrive import *
-from misc import dataset_split, extract
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--link',         required=True,  type=str,  help='gdrive link')
 parser.add_argument('--file_name',    required=True,  type=str,  help='file name')
-parser.add_argument('--ext_and_part', required=False, type=bool, default=False, help='extract and partition operation needed or not')
 
 opt = parser.parse_args()
 
 # download compressed file
 download_file_from_google_drive(opt.link, opt.file_name)
-
-if opt.ext_and_part:
-    # extract compressed file
-    extract(opt.file_name, opt.file_name[: opt.file_name.index('.')])
-
-    # data partitioning
-    dataset_split(opt.file_name[: opt.file_name.index('.')])
